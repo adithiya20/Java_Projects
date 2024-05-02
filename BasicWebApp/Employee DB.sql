ede6f721fb51eb76917c379e09522444f811452a
Adithiya Gandhi <adithiya20@gmail.com>
2024-05-02 09:24:54 +0530

    BasicWebApp project created

---
+++ BasicWebApp/Employee DB.sql
@@ -0,0 +1,16 @@
+USE employees;
+CREATE TABLE employee(
+id int(3) NOT NULL,
+first_name VARCHAR(20) DEFAULT NULL,
+last_name VARCHAR(20) DEFAULT NULL,
+username VARCHAR(250) DEFAULT NULL,
+pass_word VARCHAR(20) DEFAULT NULL,
+address VARCHAR(45) DEFAULT NULL,
+contact VARCHAR(45) DEFAULT NULL,
+PRIMARY KEY (id)
+);
+
+#describes the employee table
+DESCRIBE employee;
+
+SELECT * FROM employee;
